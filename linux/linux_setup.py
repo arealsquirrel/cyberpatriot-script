b60c1c1fbb3a1ead94c7a8a0c5f084511166f379
arealsquirrel <sophielutkenhaus@gmail.com>
2026-09-23 13:09:55 -0600

    maybe

--- linux/linux_setup.py
+++ linux/linux_setup.py
@@ -27,15 +27,15 @@ def setup_linux():
 
         if "n" not in input("configure pam"):
             run_command(["sudo", "apt-get", "install", "libpam-cracklib"])
-            run_command(["sudo", "sed", "-i", "'s/pam_unix.so/pam_unix.so md5 remember=5 use_authtok minlen=12/'", "/etc/pam.d/common-password"])
-            run_command(["sudo", "sed", "-i", "'s/nullok//g'", "/etc/pam.d/common-auth"])
+            run_command(["sudo", "sed", "-i", 's/pam_unix.so/pam_unix.so md5 remember=5 use_authtok minlen=12/', "/etc/pam.d/common-password"])
+            run_command(["sudo", "sed", "-i", 's/nullok//g', "/etc/pam.d/common-auth"])
 
-            run_command(["sudo", "sed", "-i", "'/PASS_MIN_DAYS/c\PASS_MIN_DAYS 7'", "/etc/login.defs"])
-            run_command(["sudo", "sed", "-i", "'/PASS_MAX_DAYS/c\PASS_MAX_DAYS 90'", "/etc/login.defs"])
-            run_command(["sudo", "sed", "-i", "'/PASS_WARN_AGE/c\PASS_WARN_AGE 14'", "/etc/login.defs"])
-            run_command(["sudo", "sed", "-i", "'/FAILLOG_ENAB/c\FAILLOG_ENAB YES'", "/etc/login.defs"])
-            run_command(["sudo", "sed", "-i", "'/SYSLOG_SU_ENAB/c\SYSLOG_SU_ENAB YES'", "/etc/login.defs"])
-            run_command(["sudo", "sed", "-i", "'/SYSLOG_SG_ENAB/c\SYSLOG_SG_ENAB YES'", "/etc/login.defs"])
+            run_command(["sudo", "sed", "-i", '/PASS_MIN_DAYS/c\PASS_MIN_DAYS 7', "/etc/login.defs"])
+            run_command(["sudo", "sed", "-i", '/PASS_MAX_DAYS/c\PASS_MAX_DAYS 90', "/etc/login.defs"])
+            run_command(["sudo", "sed", "-i", '/PASS_WARN_AGE/c\PASS_WARN_AGE 14', "/etc/login.defs"])
+            run_command(["sudo", "sed", "-i", '/FAILLOG_ENAB/c\FAILLOG_ENAB YES', "/etc/login.defs"])
+            run_command(["sudo", "sed", "-i", '/SYSLOG_SU_ENAB/c\SYSLOG_SU_ENAB YES', "/etc/login.defs"])
+            run_command(["sudo", "sed", "-i", '/SYSLOG_SG_ENAB/c\SYSLOG_SG_ENAB YES', "/etc/login.defs"])
 
             run_command(["sudo", "passwd", "-l", "root"])
             run_command(["sudo", "chown", "root:root", "/etc/passwd"])
